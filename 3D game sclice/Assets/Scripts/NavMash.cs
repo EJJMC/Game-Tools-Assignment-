d86beb13ab63a3d5fbbb24bae614427bf28cdd0c
EJJMC <44029567+EJJMC@users.noreply.github.com>
2018-12-08 16:29:34 +0000

    nav mesh
    tested out basic nav mesh

---
+++ 3D game sclice/Assets/Scripts/NavMash.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMash : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform _destinations;
+
+    NavMeshAgent _navMeshAgent;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        _navMeshAgent = this.GetComponent<NavMeshAgent>(); //gets nav mesh
+
+        SetDestination();
+
+
+
+
+    }
+
+    private void SetDestination()
+    {
+        if(_destinations != null)
+        {
+            Vector3 targetVector = _destinations.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+        }
+    }
+}
